f1fc364686ab20c5a118a408851f300171bc650b
Ntlongg <ntlongg723@gmail.com>
2024-12-12 01:16:34 +0700

    3

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
-const connectDB = require('./config/db');
-const router = require('./routes');
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
+const connectDB =  require ('./config/db')
+const router = require('./routes')
 
-const app = express();
+const app = express()
 app.use(cors({
-    origin: process.env.FONTEND_URL,
-    credentials: true
-}));
-app.use(express.json());
-app.use(cookieParser());
-app.use("/api", router);
+    origin : process.env.FONTEND_URL,
+    credentials : true
+}))
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api", router) 
 
-const PORT = process.env.PORT || 7203; // Sửa cách gán giá trị PORT
+const PORT = 7203 || process.env.PORT
 
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log('Server is running on port ' + PORT);
-        console.log("connect to db");
-    }).on('error', (err) => {
-        console.error('Error starting server:', err);
-    });
-}).catch((err) => {
-    console.error('Error connecting to database:', err);
-});
+connectDB().then(()=>{
+    app.listen(PORT, ()=>{
+        console.log("connect to db")
+        console.log("long dep trai")
+    })
+})
