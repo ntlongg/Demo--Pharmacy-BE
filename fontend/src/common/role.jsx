fdf4c02976c2a9792cf4c9c385e0783c3dd7b373
Ntlongg <ntlongg723@gmail.com>
2024-12-11 19:58:10 +0700

    first commit

--- fontend/src/common/role.jsx
+++
@@ -1,6 +0,0 @@
-const ROLE = {
-    ADMIN : "ADMIN",
-    PERSONNEL : "PERSONNEL",
-    CUSTOMER : "CUSTOMER"
-}
-export default ROLE
--- fontend/src/helpers/classifyCategory.jsx
+++
@@ -1,8 +0,0 @@
-const classifyCategory = [
-    { id : 1, label : "Thuốc kê đơn", value : "Thuốc kê đơn"},
-    { id : 2, label : "Thuốc không kê đơn", value : "Thuốc không kê đơn"},
-    
-]
-
-
-export default classifyCategory
